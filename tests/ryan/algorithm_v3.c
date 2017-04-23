0e61dff8e45d6d1eec6b751abdf3ec8ebb037bbc
RyanKlugman <RyanKlugman@users.noreply.github.com>
2017-04-24 03:08:03 +1000

    Fixed all invalid instruction errors

--- tests/ryan/algorithm_v3.c
+++ tests/ryan/algorithm_v3.c
@@ -4,29 +4,27 @@
 
 #include "../irq.h"
 
-uint64_t needle[64];
-size_t needleSize;
+uint64_t needle[64] = {0};
+size_t needleSize = 0;
 
 uint64_t bitArray;
 
 extern uint64_t results;
+
 
 void readHaystack(uint64_t data) {
 
     if (data > 0xff)
         exit(0);
 
-    if (data == needle[0]) {
-        bitArray |= 1; // bitArray[0] = 1
-    }
+    bitArray |= (data == needle[0]); // if (data == needle[0]) bitArray[0] = 1
 
     for (size_t i = 1; i < needleSize; ++i) {
-        bitArray &= ~((data != needle[i]) << i); // bitArray[i] &= (c == needle[i])
+        bitArray &= ~((uint64_t)(data != needle[i]) << i); // bitArray[i] &= (c == needle[i])
     }
 
-    if (bitArray >> (needleSize - 1) & 1) { // if (bitArray[needleSize - 1] == 1)
-        ++results;
-    }
+    uint64_t tmp = needleSize - 1;
+    results += (bitArray >> tmp & 1); // if (bitArray[needleSize - 1] == 1) results++
 
     bitArray <<= 1;
 
